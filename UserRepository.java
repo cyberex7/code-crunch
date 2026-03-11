3f86042f1085a7b18bd051c562fcb1ce2d4ec9ef
Ela Bhattacharya <55341845+cyberex7@users.noreply.github.com>
2026-03-11 10:32:41 +0530

    Add files via upload

---
+++ UserRepository.java
@@ -0,0 +1,7 @@
+package com.codebind.fullstack_backend.repository;
+
+import com.codebind.fullstack_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
